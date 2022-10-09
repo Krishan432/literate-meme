746a34b76392b96a419969e49948166b431d50bf
Krishan432 <81530947+Krishan432@users.noreply.github.com>
2022-10-09 18:45:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function preload(){
+}
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300)
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded(){
+    console.log("PoseNet Is Initialized.");
+}
+function draw(){
+    image(video, 0, 0, 300, 300);
+}
+function takeSnapshot(){
+    save("clownimage.png");
+}
+function gotPoses(results){
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("The nose x coordinate is "+results[0].pose.nose.x);
+        console.log("The nose y coordinate is "+results[0].pose.nose.y);
+    }
+}
